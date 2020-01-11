d6f43a2219e3ca8325bd7f139d9d37898677e0ad
Bee <pizzaman8099@gmail.com>
2020-01-11 15:50:48 -0800

    Run stagehand

---
+++ example/dart_jsx_example.dart
@@ -0,0 +1,6 @@
+import 'package:dart_jsx/dart_jsx.dart';
+
+main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ lib/dart_jsx.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library dart_jsx;
+
+export 'src/dart_jsx_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
